577fb420a3ddcb81ab7e07ca0b11af181dd9b0c4
matheus621 <matheus621cardoso@gmail.com>
2019-05-07 16:59:17 -0300

    Update RequestStage.java

--- src/main/java/com/springcourse/domain/RequestStage.java
+++ src/main/java/com/springcourse/domain/RequestStage.java
@@ -1,5 +1,22 @@
 package com.springcourse.domain;
 
-public class RequestStage {
+import java.util.Date;
 
+import com.springcourse.domain.enums.RequestState;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class RequestStage {
+	private Long id;
+	private Date realizaDate;
+	private String descripiton;	
+	private RequestState state;
+	private Request request;
+	private User user;
 }

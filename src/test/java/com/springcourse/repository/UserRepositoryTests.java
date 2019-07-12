9b5ef3108194ff396681891b0d3242e55cd258e4
matheus621 <matheus621cardoso@gmail.com>
2019-07-12 16:17:02 -0300

    Create user repository tests

--- src/test/java/com/springcourse/repository/UserRepositoryTests.java
+++ src/test/java/com/springcourse/repository/UserRepositoryTests.java
@@ -1,22 +1,31 @@
 package com.springcourse.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import java.util.Optional;
 
+import org.junit.FixMethodOrder;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.expression.spel.ast.OpInc;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.springcourse.domain.User;
 import com.springcourse.domain.enums.Role;
 
 @RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @SpringBootTest
 public class UserRepositoryTests {
 	
 	@Autowired private UserRepository userRepository;
 	
-	public void saveTest() {
+	@Test
+	public void AsaveTest() {
 		User user = new User(null, "Matheus", "matheus621cardoso@gmail.com", "123", Role.ADMINISTRATOR, null, null);
 		User createdUser = userRepository.save(user);
 		
@@ -24,13 +33,15 @@ public class UserRepositoryTests {
 		
 	}
 	
+	@Test
 	public void updateTest() {
-		User user = new User(1L,"Matheus Cardoso", "matheus621cardoso@gmail.com", "123", Role.ADMINISTRATOR, null, null)
+		User user = new User(1L,"Matheus Cardoso", "matheus621cardoso@gmail.com", "123", Role.ADMINISTRATOR, null, null);
 		User updateUser = userRepository.save(user);
 		
 		assertThat(updateUser.getName()).isEqualTo("Matheus Cardoso");
 	}
 	
+	@Test
 	public void getByIdTest() {
 		Optional<User> result = userRepository.findById(1L);	
 		User user = result.get();
@@ -38,12 +49,19 @@ public class UserRepositoryTests {
 		assertThat(user.getPassword()).isEqualTo("123");
 	}
 	
+	@Test
 	public void listTest() {
+		List<User> users = userRepository.findAll();
 		
+		assertThat(users.size()).isEqualTo(1);
 	}
 	
+	@Test
 	public void loginTest() {
+		Optional<User> result = userRepository.login("matheus621cardoso@gmail.com", "123");
+		User loggedUser = result.get();
 		
+		assertThat(loggedUser.getId()).isEqualTo(1L);
 	}
 
 }

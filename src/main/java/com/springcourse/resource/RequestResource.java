943aaae23f0bb3a61929ffdfca788faa30ad201d
matheus621 <matheus621cardoso@gmail.com>
2019-07-17 08:46:40 -0300

    Revert "Create request stage resource"
    This reverts commit 511c97bb5c9b243b4913194d0abe05295c73cabe.

--- src/main/java/com/springcourse/resource/RequestResource.java
+++ src/main/java/com/springcourse/resource/RequestResource.java
@@ -14,15 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springcourse.domain.Request;
-import com.springcourse.domain.RequestStage;
 import com.springcourse.service.RequestService;
-import com.springcourse.service.RequestStageService;
 
 @RestController
 @RequestMapping(value = "requests")
 public class RequestResource {
 	@Autowired private RequestService requestService;
-	@Autowired private RequestStageService stageService;
 	
 	@PostMapping
 	public ResponseEntity<Request> save(@RequestBody Request request){
@@ -50,9 +47,4 @@ public class RequestResource {
 		return ResponseEntity.ok(requests);
 	}
 	
-	@GetMapping("/{id}/request-stages")
-	public ResponseEntity<List<RequestStage>> listAllStagesById(@PathVariable (name = "id") Long id){
-		List<RequestStage> stages = stageService.listAllByRequestId(id);
-		return ResponseEntity.ok(stages);
-	}
 }
